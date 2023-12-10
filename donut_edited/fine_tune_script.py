c5b562ac6fcfcdcf36f3f299cc34a9b8443be347
Apoorva Kumar <akumar255@wisc.edu>
2023-12-09 23:06:14 -0600

    Removed Testing Code for training

--- donut_edited/fine_tune_script.py
+++ donut_edited/fine_tune_script.py
@@ -479,75 +479,75 @@ trainer.fit(model_module)
 Here we're just loading from the main branch, which means the latest commit.
 """
 
-from transformers import DonutProcessor, VisionEncoderDecoderModel
+# from transformers import DonutProcessor, VisionEncoderDecoderModel
 
-processor = DonutProcessor.from_pretrained("nielsr/donut-demo")
-model = VisionEncoderDecoderModel.from_pretrained("nielsr/donut-demo")
+# processor = DonutProcessor.from_pretrained("nielsr/donut-demo")
+# model = VisionEncoderDecoderModel.from_pretrained("nielsr/donut-demo")
 
 """As we don't have a test split here, let's evaluate on the validation split.
 
 We'll use the `token2json` method of the processor to turn the generated sequences into JSON, and the `JSONParseEvaluator` object available in the Donut package.
 """
 
-import re
-import json
-import torch
-from tqdm.auto import tqdm
-import numpy as np
+# import re
+# import json
+# import torch
+# from tqdm.auto import tqdm
+# import numpy as np
 
-from donut import JSONParseEvaluator
+# from donut import JSONParseEvaluator
 
-from datasets import load_dataset
+# from datasets import load_dataset
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+# device = "cuda" if torch.cuda.is_available() else "cpu"
 
-model.eval()
-model.to(device)
+# model.eval()
+# model.to(device)
 
-output_list = []
-accs = []
+# output_list = []
+# accs = []
 
-dataset = load_dataset("naver-clova-ix/cord-v2", split="validation")
+# dataset = load_dataset("naver-clova-ix/cord-v2", split="validation")
 
-for idx, sample in tqdm(enumerate(dataset), total=len(dataset)):
-    # prepare encoder inputs
-    pixel_values = processor(sample["image"].convert("RGB"), return_tensors="pt").pixel_values
-    pixel_values = pixel_values.to(device)
-    # prepare decoder inputs
-    task_prompt = "<s_cord-v2>"
-    decoder_input_ids = processor.tokenizer(task_prompt, add_special_tokens=False, return_tensors="pt").input_ids
-    decoder_input_ids = decoder_input_ids.to(device)
+# for idx, sample in tqdm(enumerate(dataset), total=len(dataset)):
+#     # prepare encoder inputs
+#     pixel_values = processor(sample["image"].convert("RGB"), return_tensors="pt").pixel_values
+#     pixel_values = pixel_values.to(device)
+#     # prepare decoder inputs
+#     task_prompt = "<s_cord-v2>"
+#     decoder_input_ids = processor.tokenizer(task_prompt, add_special_tokens=False, return_tensors="pt").input_ids
+#     decoder_input_ids = decoder_input_ids.to(device)
 
-    # autoregressively generate sequence
-    outputs = model.generate(
-            pixel_values,
-            decoder_input_ids=decoder_input_ids,
-            max_length=model.decoder.config.max_position_embeddings,
-            early_stopping=True,
-            pad_token_id=processor.tokenizer.pad_token_id,
-            eos_token_id=processor.tokenizer.eos_token_id,
-            use_cache=True,
-            num_beams=1,
-            bad_words_ids=[[processor.tokenizer.unk_token_id]],
-            return_dict_in_generate=True,
-        )
+#     # autoregressively generate sequence
+#     outputs = model.generate(
+#             pixel_values,
+#             decoder_input_ids=decoder_input_ids,
+#             max_length=model.decoder.config.max_position_embeddings,
+#             early_stopping=True,
+#             pad_token_id=processor.tokenizer.pad_token_id,
+#             eos_token_id=processor.tokenizer.eos_token_id,
+#             use_cache=True,
+#             num_beams=1,
+#             bad_words_ids=[[processor.tokenizer.unk_token_id]],
+#             return_dict_in_generate=True,
+#         )
 
-    # turn into JSON
-    seq = processor.batch_decode(outputs.sequences)[0]
-    seq = seq.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
-    seq = re.sub(r"<.*?>", "", seq, count=1).strip()  # remove first task start token
-    seq = processor.token2json(seq)
+#     # turn into JSON
+#     seq = processor.batch_decode(outputs.sequences)[0]
+#     seq = seq.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
+#     seq = re.sub(r"<.*?>", "", seq, count=1).strip()  # remove first task start token
+#     seq = processor.token2json(seq)
 
-    ground_truth = json.loads(sample["ground_truth"])
-    ground_truth = ground_truth["gt_parse"]
-    evaluator = JSONParseEvaluator()
-    score = evaluator.cal_acc(seq, ground_truth)
+#     ground_truth = json.loads(sample["ground_truth"])
+#     ground_truth = ground_truth["gt_parse"]
+#     evaluator = JSONParseEvaluator()
+#     score = evaluator.cal_acc(seq, ground_truth)
 
-    accs.append(score)
-    output_list.append(seq)
+#     accs.append(score)
+#     output_list.append(seq)
 
-scores = {"accuracies": accs, "mean_accuracy": np.mean(accs)}
-print(scores, f"length : {len(accs)}")
+# scores = {"accuracies": accs, "mean_accuracy": np.mean(accs)}
+# print(scores, f"length : {len(accs)}")
 
-print("Mean accuracy:", np.mean(accs))
+# print("Mean accuracy:", np.mean(accs))
 
