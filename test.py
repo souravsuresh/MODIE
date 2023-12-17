124bea8a1ff15edc1f01e8524089b0e34bdc129f
Apoorva Kumar <akumar255@wisc.edu>
2023-12-17 14:43:58 -0600

    Added Inference script

---
+++ test.py
@@ -0,0 +1,227 @@
+# -*- coding: utf-8 -*-
+
+import json
+import numpy as np
+import cv2
+import scipy.ndimage
+import re
+import matplotlib.pyplot as plt
+from PIL import ImageOps, Image
+from ast import literal_eval
+from typing import Any, List, Tuple
+from nltk import edit_distance
+
+from datasets import load_dataset
+from transformers import DonutProcessor, VisionEncoderDecoderModel, VisionEncoderDecoderConfig
+
+import torch
+import pytorch_lightning as pl
+from utils.dataset import DonutDataset
+from utils.util import prepare_input
+
+from donut import JSONParseEvaluator
+from tqdm.auto import tqdm
+from sconf import Config
+
+
+dataset = load_dataset("naver-clova-ix/cord-v2")
+config = Config("config/train_cord.yaml")
+
+# update image_size of the encoder
+# during pre-training, a larger image size was used
+encdecconfig = VisionEncoderDecoderConfig.from_pretrained(config.pretrained_model_name_or_path)
+encdecconfig.encoder.image_size = config.input_size # (height, width)
+
+# update max_length of the decoder (for generation)
+encdecconfig.decoder.max_length = config.max_length
+
+# instantiate the model with our custom config, as well as the processor. 
+# Make sure that all pre-trained weights are correctly loaded (a warning would tell you if that's not the case)
+processor = DonutProcessor.from_pretrained(config.pretrained_model_name_or_path)
+model = VisionEncoderDecoderModel.from_pretrained(config.pretrained_model_name_or_path, config=encdecconfig)
+
+# we update some settings which differ from pretraining; namely the size of the images + no rotation required
+# source: https://github.com/clovaai/donut/blob/master/config/train_cord.yaml
+processor.image_processor.size = config.input_size[::-1] # should be (width, height)
+processor.image_processor.do_align_long_axis = False
+
+val_dataset = DonutDataset("naver-clova-ix/cord-v2", config=config,
+                             split="validation", task_start_token="<s_cord-v2>", prompt_end_token="<s_cord-v2>",
+                             sort_json_key=config.sort_json_key, # cord dataset is preprocessed, so no need for this
+                             processor = processor,
+                             model = model,
+                             )
+
+# the vocab size attribute stays constants (might be a bit unintuitive - but doesn't include special tokens)
+print("Original number of tokens:", processor.tokenizer.vocab_size)
+print("Number of tokens after adding special tokens:", len(processor.tokenizer))
+
+
+model.config.pad_token_id = processor.tokenizer.pad_token_id
+model.config.decoder_start_token_id = processor.tokenizer.convert_tokens_to_ids(['<s_cord-v2>'])[0]
+
+# sanity check
+print("Pad token ID:", processor.decode([model.config.pad_token_id]))
+print("Decoder start token ID:", processor.decode([model.config.decoder_start_token_id]))
+
+
+class DonutModelPLModule(pl.LightningModule):
+    def __init__(self, config, processor, model):
+        super().__init__()
+        self.config = config
+        self.processor = processor
+        self.model = model
+
+    def training_step(self, batch, batch_idx):
+        pixel_values, labels, _ = batch
+
+        outputs = self.model(pixel_values, labels=labels)
+        loss = outputs.loss
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx, dataset_idx=0):
+        pixel_values, labels, answers = batch
+        batch_size = pixel_values.shape[0]
+        # we feed the prompt to the model
+        decoder_input_ids = torch.full((batch_size, 1), self.model.config.decoder_start_token_id, device=self.device)
+
+        outputs = self.model.generate(pixel_values,
+                                   decoder_input_ids=decoder_input_ids,
+                                   max_length=self.config.max_length,
+                                   early_stopping=True,
+                                   pad_token_id=self.processor.tokenizer.pad_token_id,
+                                   eos_token_id=self.processor.tokenizer.eos_token_id,
+                                   use_cache=True,
+                                   num_beams=1,
+                                   bad_words_ids=[[self.processor.tokenizer.unk_token_id]],
+                                   return_dict_in_generate=True,)
+
+        predictions = []
+        for seq in self.processor.tokenizer.batch_decode(outputs.sequences):
+            seq = seq.replace(self.processor.tokenizer.eos_token, "").replace(self.processor.tokenizer.pad_token, "")
+            seq = re.sub(r"<.*?>", "", seq, count=1).strip()  # remove first task start token
+            predictions.append(seq)
+
+        scores = []
+        for pred, answer in zip(predictions, answers):
+            pred = re.sub(r"(?:(?<=>) | (?=</s_))", "", pred)
+            # NOT NEEDED ANYMORE
+            # answer = re.sub(r"<.*?>", "", answer, count=1)
+            answer = answer.replace(self.processor.tokenizer.eos_token, "")
+            scores.append(edit_distance(pred, answer) / max(len(pred), len(answer)))
+
+            if self.config.get("verbose", False) and len(scores) == 1:
+                print(f"Prediction: {pred}")
+                print(f"    Answer: {answer}")
+                print(f" Normed ED: {scores[0]}")
+
+        self.log("val_edit_distance", np.mean(scores))
+
+        return scores
+
+    def configure_optimizers(self):
+        # you could also add a learning rate scheduler if you want
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.config.get("lr"))
+
+        return optimizer
+
+    def train_dataloader(self):
+        pass
+
+    def val_dataloader(self):
+        pass
+
+model_module = DonutModelPLModule.load_from_checkpoint('/results/modie_e2e_frozen_decoder_epoch=29-val_loss=0.00.ckpt', config= config, processor= processor, model = model )
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+model = model_module.model
+model.eval()
+model.to(device)
+
+output_list = []
+gt = []
+accs = []
+images = []
+blur_images = []
+
+dataset = load_dataset("naver-clova-ix/cord-v2", split="validation")
+
+for idx, sample in tqdm(enumerate(dataset), total=len(dataset)):
+    # prepare encoder inputs
+    img = sample['image']
+    blur_img = prepare_input(img, config.input_size, add_blur=True)
+    pixel_values = processor(blur_img.convert("RGB"), return_tensors="pt").pixel_values
+    pixel_values = pixel_values.to(device)
+    # prepare decoder inputs
+    task_prompt = "<s_cord-v2>"
+    decoder_input_ids = processor.tokenizer(task_prompt, add_special_tokens=False, return_tensors="pt").input_ids
+    decoder_input_ids = decoder_input_ids.to(device)
+
+    # autoregressively generate sequence
+    outputs = model.generate(
+            pixel_values,
+            decoder_input_ids=decoder_input_ids,
+            max_length=model.decoder.config.max_position_embeddings,
+            early_stopping=True,
+            pad_token_id=processor.tokenizer.pad_token_id,
+            eos_token_id=processor.tokenizer.eos_token_id,
+            use_cache=True,
+            num_beams=1,
+            bad_words_ids=[[processor.tokenizer.unk_token_id]],
+            return_dict_in_generate=True,
+        )
+
+    # turn into JSON
+    seq = processor.batch_decode(outputs.sequences)[0]
+    seq = seq.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
+    seq = re.sub(r"<.*?>", "", seq, count=1).strip()  # remove first task start token
+    seq = processor.token2json(seq)
+
+    ground_truth = json.loads(sample["ground_truth"])
+    ground_truth = ground_truth["gt_parse"]
+    evaluator = JSONParseEvaluator()
+    score = evaluator.cal_acc(seq, ground_truth)
+
+    accs.append(score)
+    output_list.append(seq)
+    gt.append(ground_truth)
+    images.append(img)
+    blur_images.append(blur_img)
+
+scores = {"accuracies": accs, "mean_accuracy": np.mean(accs)}
+print(scores, f"length : {len(accs)}")
+
+print("Mean accuracy:", np.mean(accs))
+
+def inf(img):
+    # img = Image.open('test.jpeg')
+    pixel_values = processor(img.convert("RGB"), return_tensors="pt").pixel_values
+    pixel_values = pixel_values.to(device)
+    # prepare decoder inputs
+    task_prompt = "<s_cord-v2>"
+    decoder_input_ids = processor.tokenizer(task_prompt, add_special_tokens=False, return_tensors="pt").input_ids
+    decoder_input_ids = decoder_input_ids.to(device)
+
+    # autoregressively generate sequence
+    outputs = model.generate(
+            pixel_values,
+            decoder_input_ids=decoder_input_ids,
+            max_length=model.decoder.config.max_position_embeddings,
+            early_stopping=True,
+            pad_token_id=processor.tokenizer.pad_token_id,
+            eos_token_id=processor.tokenizer.eos_token_id,
+            use_cache=True,
+            num_beams=1,
+            bad_words_ids=[[processor.tokenizer.unk_token_id]],
+            return_dict_in_generate=True,
+        )
+
+    # turn into JSON
+    seq = processor.batch_decode(outputs.sequences)[0]
+    seq = seq.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
+    seq = re.sub(r"<.*?>", "", seq, count=1).strip()  # remove first task start token
+    seq = processor.token2json(seq)
+
+    return seq
